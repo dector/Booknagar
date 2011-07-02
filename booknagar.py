170f9ff82d771720693df93d3f513601da322dec
dector <dector9@gmail.com>
2011-07-02 23:26:17 +0300

    Redesigned and practically used first version

--- booknagar.py
+++ booknagar.py
@@ -4,41 +4,27 @@ listsInBooklete = 5
 PAGES_ON_LIST = 4
 PAGES_IN_BOOKLETE = listsInBooklete * PAGES_ON_LIST
 
-pagesNumber = 80
+chapNumber = 19
+pagesNumber = 14
+
 currentBookleteNumber = 1
 
 while (currentBookleteNumber-1) * PAGES_IN_BOOKLETE < pagesNumber :
-    currentPage = (currentBookleteNumber-1) * PAGES_IN_BOOKLETE + 1
-    evenPagesList = []
-    oddPagesList = []
+    execStr = "psselect -p "
 
-    while currentPage <= PAGES_IN_BOOKLETE * currentBookleteNumber :
-        if currentPage <= pagesNumber :
-            if currentPage % 2 == 1 :
-                evenPagesList.append(currentPage)
-            else :
-                oddPagesList.append(currentPage)
-        currentPage += 1
+    maxPage = PAGES_IN_BOOKLETE * currentBookleteNumber
+    if maxPage > pagesNumber :
+        maxPage = pagesNumber
 
-    print "=== Booklete #" + str(currentBookleteNumber) + " ===";
+    minPage = (currentBookleteNumber-1) * PAGES_IN_BOOKLETE + 1
 
-    s = "All pages: " + str((currentBookleteNumber-1) * PAGES_IN_BOOKLETE + 1) + "-"
-    s += str(PAGES_IN_BOOKLETE * currentBookleteNumber)
-    print s
-    #for evenPage in evenPagesList :
-    #    s += str(evenPage) + ","
-    #for oddPage in oddPagesList :
-    #    s += str(oddPage) + ","
-    #print s[:-1]
+    chapStr = str(chapNumber) + "_" + str(currentBookleteNumber)
 
-    s = "Even pages: "
-    for evenPage in evenPagesList :
-        s += str(evenPage) + ","
-    print s[:-1]
+    execStr += str(minPage) + "-" + str(maxPage) + " chap" + str(chapNumber) + ".ps "
+    execStr += "chap" + chapStr + ".ps; psbook chap" + chapStr + ".ps | psnup -2 -l -p a4 > print"
+    execStr += chapStr + ".ps; rm chap" + chapStr + ".ps"
+    print execStr
 
-    s = "Odd pages: "
-    for oddPage in oddPagesList :
-        s += str(oddPage) + ","
-    print s[:-1]
- 
     currentBookleteNumber += 1
+
+print "mkdir " + str(chapNumber) + "; rm -f chap" + str(chapNumber) + "*" + "; mv print" + str(chapNumber) + "_* " + str(chapNumber)
